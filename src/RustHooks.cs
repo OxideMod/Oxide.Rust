219d261798a4de5f238ecd1b7c678d40d63c00ca
Luke Spragg <luke@spragg.me>
2019-12-06 19:43:59 -0500

    Fix compiling due to missing using

--- src/RustHooks.cs
+++ src/RustHooks.cs
@@ -6,6 +6,7 @@ using Oxide.Core.Libraries.Covalence;
 using Oxide.Core.Plugins;
 using Rust.Ai;
 using Rust.Ai.HTN;
+using Steamworks;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;

2fc49fa9c6beea70243262947e24b6e26934114b
Luke Spragg <luke@spragg.me>
2020-09-04 14:14:20 -0400

    Make sure OnServerInitialized is called after Steam is connected

--- src/RustCore.cs
+++ src/RustCore.cs
@@ -304,7 +304,7 @@ namespace Oxide.Game.Rust
                 {
                     return activePlayer;
                 }
-                
+
                 if (activePlayer.net?.connection != null && activePlayer.net.ID.Equals(nameOrIdOrIp))
                 {
                     return activePlayer;
